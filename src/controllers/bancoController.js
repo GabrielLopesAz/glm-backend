21304c46f20e73fb2ceb207f5b290b322c5778db
Gabriel Lopes <54581670+GabrielLopesAz@users.noreply.github.com>
2021-06-10 23:29:33 -0300

    Delete src directory

--- src/controllers/bancoController.js
+++
@@ -1,9 +0,0 @@
-class bancoController{
-
-  async create (req,res){
-    console.log(req.body);
-    res.send("Pegando corpo da requisição")
-  }
-}
-
-module.exports = new bancoController();
--- src/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express()
-
-app.get("/", function(req,res){
-  res.send("welcome to my page")
-})
-app.listen(8888,()=>{
-  console.log("API ON")
-})
